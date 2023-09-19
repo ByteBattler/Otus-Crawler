d1270dc3b715793c1ea0f2dfdbf761eee3307c36
Alexey Tatarintsev <lex.tatarintsev@gmail.com>
2023-09-19 22:14:48 +1000

    changed some files

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -1,10 +1,15 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.35"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "= 3.1.1"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.00"
 }
 
 provider "yandex" {
